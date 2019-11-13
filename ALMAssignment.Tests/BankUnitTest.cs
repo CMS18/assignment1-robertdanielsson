54b23dc5ec550887acadb71e6797d1ac6e12f2d9
Robert Danielsson <robert.danielsson@yh.nackademin.se>
2019-11-13 14:50:37 +0100

    renamed unittest class

--- ALMAssignment.Tests/BankUnitTest.cs
+++ ALMAssignment.Tests/BankUnitTest.cs
@@ -3,7 +3,7 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 namespace ALMAssignment.Tests
 {
     [TestClass]
-    public class UnitTest1
+    public class BankUnitTest
     {
         [TestMethod]
         public void TestMethod1()
