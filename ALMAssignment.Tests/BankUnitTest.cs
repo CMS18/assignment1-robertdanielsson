a076baa52b19350dd15aff8acde4493e64c81dad
Robert Danielsson <robert.danielsson@yh.nackademin.se>
2019-11-13 15:17:44 +0100

    Withdrawal and deposit tests done, added AccountController

--- ALMAssignment.Tests/BankUnitTest.cs
+++ ALMAssignment.Tests/BankUnitTest.cs
@@ -1,4 +1,6 @@
+using ALMAssignment.WebUI.Models;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
 
 namespace ALMAssignment.Tests
 {
@@ -6,8 +8,86 @@ namespace ALMAssignment.Tests
     public class BankUnitTest
     {
         [TestMethod]
-        public void TestMethod1()
+        public void Deposit()
         {
+            //Arrange
+            decimal depositAmount = 20m;
+
+            //Act
+            var account = new Account();
+            account.Deposit(depositAmount);
+            var actualAmount = account.Balance;
+
+            //Assert
+            Assert.AreEqual(depositAmount, actualAmount);
         }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentOutOfRangeException))]
+        public void DepositNegative()
+        {
+            //Arrange
+            decimal depositAmount = -20m;
+
+            //Act
+            var account = new Account();
+            account.Deposit(depositAmount);
+            var actualAmount = account.Balance;
+
+            //Assert ArgumentOutOfRangeException
+        }
+
+        [TestMethod]
+        public void Withdraw()
+        {
+            //Arrange
+            decimal withdrawAmount = 20m;
+            decimal expectedAmount = 5m;
+
+            //Act
+            var account = new Account();
+            account.Balance = 25m;
+            account.Withdraw(withdrawAmount);
+            var actualAmount = account.Balance;
+
+            //Assert
+            Assert.AreEqual(expectedAmount, actualAmount);
+        }
+
+        [TestMethod]
+        public void WithdrawAll()
+        {
+            //Arrange
+            decimal withdrawAmount = 20m;
+            decimal expectedAmount = 0m;
+
+            //Act
+            var account = new Account();
+            account.Balance = 20m;
+            account.Withdraw(withdrawAmount);
+            var actualAmount = account.Balance;
+
+            //Assert
+            Assert.AreEqual(expectedAmount, actualAmount);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentOutOfRangeException))]
+        public void WithdrawExceeded()
+        {
+            //Arrange
+            decimal withdrawAmount = 20m;
+
+            //Act
+            var account = new Account();
+            account.Balance = 19.99m;
+            account.Withdraw(withdrawAmount);
+
+            //Assert ArgumentOutOfRangeException
+        }
+
+        //Arrange
+        //Act
+        //Assert
     }
 }
